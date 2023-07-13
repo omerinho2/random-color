4f5dbdf864ff07507c9993a550c27816bbc5b217
pofins <omerefe369@gmail.com>
2023-07-13 17:26:47 +0300

    random colors
    öyleli

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React , {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [hex , setHex] = useState("#ffffff");
+
+  function randomHex(){
+    const randomhex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomhex)
+  }
+
+  return (
+    <div className="App" style={{backgroundColor:`${hex}`}}>
+     <h1>{hex}</h1>
+     <button onClick={randomHex}>click for new Backround</button>
+     <button onClick={()=> navigator.clipboard.writeText(hex)}>copy the hex value</button>
+    </div>
+  );
+}
+
+export default App;
